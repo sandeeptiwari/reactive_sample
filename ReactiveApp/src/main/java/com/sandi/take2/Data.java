80cf5c0cd90e6585994b984012d5477a37e16b98
sandeeptiwari <sandeep15mca@gmail.com>
2018-08-28 21:30:36 +0530

    take2

---
+++ ReactiveApp/src/main/java/com/sandi/take2/Data.java
@@ -0,0 +1,58 @@
+package com.sandi.take2;
+
+public class Data {
+	private double price = 0.0;
+	private String productName = null;
+
+
+	public Data(double price, String productName) {
+		this.price = price;
+		this.productName = productName;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(price);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((productName == null) ? 0 : productName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Data other = (Data) obj;
+		if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
+			return false;
+		if (productName == null) {
+			if (other.productName != null)
+				return false;
+		} else if (!productName.equals(other.productName))
+			return false;
+		return true;
+	}
+
+
+
+}
---
+++ ReactiveApp/src/main/java/com/sandi/take2/Driver.java
@@ -0,0 +1,47 @@
+package com.sandi.take2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.ToDoubleFunction;
+
+public class Driver {
+
+	public static void main(String[] args) throws IOException {
+		List<Data> datas = Collections.synchronizedList(new ArrayList<Data>());
+
+		//reader input datas
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		Data data = null;
+		String productName = null;
+		double price = 0.0;
+		System.out.println("Reactive style");
+		while(true) {
+			System.out.println("Please enter name -> ");
+			productName = reader.readLine();
+			if(productName.equals("exit")) break;
+			else if(productName.isEmpty()) continue;
+
+
+			System.out.println("Please enter value -> ");
+			try {
+				price = Double.valueOf(reader.readLine());
+			}catch (NumberFormatException e) {
+			}
+
+			data = new Data(price, productName);
+
+			if(datas.contains(data))
+				datas.get(datas.indexOf(data)).setPrice(data.getPrice());
+			else
+				datas.add(data);
+
+			System.out.println(datas.stream().mapToDouble(value -> value.getPrice()).sum());
+		}
+	}
+
+}
